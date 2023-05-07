92f39aa4910b7fdb8eb8e32de86e94a09917c501
Jhonatan S. Souza <jhonatan2760@gmail.com>
2023-05-07 20:43:00 -0300

    Update configuration

---
+++ src/main/kotlin/br/com/jhonatansouza/designfactory/designfactory/DesignFactoryApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jhonatansouza.designfactory.designfactory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesignFactoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesignFactoryApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/jhonatansouza/designfactory/designfactory/DesignFactoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jhonatansouza.designfactory.designfactory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesignFactoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
